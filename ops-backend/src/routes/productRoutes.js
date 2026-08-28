90252bb9a6a565bdb0de2759a22324e5a51facbd
rupeshbhoir-zen <rupesh.bhoir@zensar.com>
2026-08-28 12:00:08 +0530

    product service v1

--- ops-backend/src/routes/productRoutes.js
+++ ops-backend/src/routes/productRoutes.js
@@ -1,12 +1,23 @@
 import express from "express";
+import  {getProductData} from "../services/productService.js"; //ECMA SCript
+// const abc = require("../services/productService");
 
 // export const productRoute = express.Router();  //named export
 const productRoute = express.Router(); 
 
-productRoute.get('/product', (req,res,next)=> {
+productRoute.get('/simple-product', (req,res,next)=> {
     console.log("Product fetching ");
     res.status(200).json({msg:"GET - I am in productRoutes.js file"});
     // next();   //control will pass to the matching route handler i.e. present in your index.js
 });
+
+productRoute.get('/product', async(req,res)=> {
+    // console.log("I am in GET productRoutes.js")
+    const result = getProductData();
+    // const result = await getProductData();
+    console.log(result);
+    res.send(result);
+});
+
 
 export default productRoute;                      //default export
---
+++ ops-backend/src/services/productService.js
@@ -0,0 +1,9 @@
+// Filepath: ops-backend\src\services\productService.js
+function getProductData(){
+    return (
+        {id:1, name:"Laptop L101", category:"electronics", subcategory:"laptop", "availableQty":4, price:88000.56, }
+    );
+}
+
+
+export { getProductData };
